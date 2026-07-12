c8ad3db57a94568697a1f0018cadfe4ea3e16c76
herukanz-12 <heru.kanz12@gmail.com>
2026-07-12 22:29:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Ganti string ini dengan URL Web App GAS kamu setelah Deploy > New deployment > Web app.
 // Contoh: https://script.google.com/macros/s/AKfycb..../exec
-const API_URL = 'PASTE_URL_WEB_APP_GAS_DI_SINI';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyf1DEEMivzjYTgDcyoZDXhMO694weUEjQ0zUpOE2-4BR_M6sZ2DU31yVqKsWcXjXuZFQ/exec';
